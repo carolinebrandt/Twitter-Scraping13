cbd92e185f1dafef4b9a437ead3a5ba340f33741
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 17:00:09 +0100

    installing pip

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,4 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
+sudo apt install python-pip
+sudo pip install pip-requirements.txt
 nohup python uppsala_aws.py &
