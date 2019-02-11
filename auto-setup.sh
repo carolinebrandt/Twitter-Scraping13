db9d7ac6c4b591893fbdb7145f1edbb5d5238a7e
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 17:18:01 +0100

    changing ownership

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
+sudo -H chown -R ubuntu: /home/ubuntu/.cache
 sudo apt install python-pip
 sudo pip install -r pip-requirements.txt
 nohup python uppsala_aws.py &
