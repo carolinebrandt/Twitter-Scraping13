a8efa24f6e96ec317c79771ea4d2d95bc18c60ad
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 11:24:05 +0100

    manually creating pip-requirements.txt

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,7 +3,4 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo pip install pip-requirements.txt
-python get-pip.py
 nohup python uppsala_aws.py &
