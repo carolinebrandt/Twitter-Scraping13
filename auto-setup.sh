d23cee5cb6ab9df124cff7cad5fb48097dad1923
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 20:39:45 +0100

    virtualenv

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,6 +3,11 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
-sudo -H apt install python-pip
-sudo pip install -r pip-requirements.txt
+
+sudo apt-get install python-pip
+sudo pip install virtualenv 
+virtualenv venv
+source venv/bin/activate
+
+sudo pip install pip-requirements.txt
 nohup python uppsala_aws.py &
