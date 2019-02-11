97e21fec1075ea6b7870c4468a3bdc5a40bdabf3
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 11:12:01 +0100

    installing easy_instal

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
+sudo apt-get install easy_install
 sudo easy_install pip
 sudo pip install pip-requirements.txt
 nohup python uppsala_aws.py &
