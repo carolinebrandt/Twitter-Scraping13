1e455a78518a7e1a4dc818efd1b1ff50e18f3668
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 18:07:44 +0100

    Update auto-setup.sh

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
-sudo apt install python-pip
+sudo -H apt install python-pip
 #sudo -H pip install -r pip-requirements.txt
 sudo -H pip install pip-requirements.txt
 nohup python uppsala_aws.py &
