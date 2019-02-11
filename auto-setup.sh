2a510a05b8324905bdfb5cad757a37a5a17242b7
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 18:06:07 +0100

    removing -r from pip

--- auto-setup.sh
+++ auto-setup.sh
@@ -4,5 +4,6 @@ sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
 sudo apt install python-pip
-sudo -H pip install -r pip-requirements.txt
+#sudo -H pip install -r pip-requirements.txt
+sudo -H pip install pip-requirements.txt
 nohup python uppsala_aws.py &
