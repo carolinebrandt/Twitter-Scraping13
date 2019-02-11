bc04308bbbed7b84aabef6644b86d70c7b99f582
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-11 11:21:21 +0100

    Intalling pip

--- auto-setup.sh
+++ auto-setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
-sudo apt-get install easy_install
-sudo easy_install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo pip install pip-requirements.txt
+python get-pip.py
 nohup python uppsala_aws.py &
