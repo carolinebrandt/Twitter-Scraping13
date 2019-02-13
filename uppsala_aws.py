2063a2f86ceef562ab4a9dce3cc7fe02a5a7b8b6
Caroline M. Brandt <cbrandt@berkeley.edu>
2019-02-13 11:49:15 +0100

    from Egypt location filter to the whole firehose

--- uppsala_aws.py
+++ uppsala_aws.py
@@ -25,9 +25,9 @@ def process_or_store(tweet):
     #f = codecs.open('tweetDump.json', 'a','utf-8') #writing to local file.
     try:
         response = firehose_client.put_record(
-            DeliveryStreamName='uppsala-twitter-stream-egypt',
+            DeliveryStreamName='uppsala-twitter-one-hour',
             Record={
-                'Data': json.dumps(tweet, ensure_ascii=False, encoding="utf-8")+'\n'
+                'Data': tweet
             }
         )
         logging.info(response)
@@ -58,14 +58,15 @@ logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
  
 def main():
   twitter_stream = Stream(auth, MyListener())
-  twitter_stream.filter(locations=[22.0, 31.8330854, 24.6499112, 37.1153517], stall_warnings=True)
-    
+  twitter_stream.sample()
+ 
+main()
 
-startTime=time.time()
-while True:
-  if __name__ == "__main__":
-    main()
-  time.sleep(1800.0 - time.time() % 60)
+#startTime=time.time()
+#while True:
+#  if __name__ == "__main__":
+#    main()
+#  time.sleep(1800.0 - time.time() % 60)
   
 
 
